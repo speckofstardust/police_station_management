0546e4be3f86ac0a5da0dc9c9c4af68b4f39366c
Bluutooth <sharmada03@gmail.com>
2024-03-04 20:12:18 +0530

    needs heavy editing

---
+++ app.py
@@ -0,0 +1,142 @@
+import mysql.connector,sys
+import datetime
+from mysql.connector import Error
+from flask import Flask, request, jsonify, render_template,redirect, url_for
+from random import randint
+
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET', 'POST'])
+def renderLoginPage():
+    events = runQuery("SELECT * FROM events")
+    branch =  runQuery("SELECT * FROM branch")
+    if request.method == 'POST':
+        Name = request.form['FirstName'] + " " + request.form['LastName']
+        Mobile = request.form['MobileNumber']
+        Branch_id = request.form['Branch']
+        Event = request.form['Event']
+        Email = request.form['Email']
+
+        if len(Mobile) != 10:
+            return render_template('loginfail.html',errors = ["Invalid Mobile Number!"])
+
+        if Email[-4:] != '.com':
+            return render_template('loginfail.html', errors = ["Invalid Email!"])
+
+        if len(runQuery("SELECT * FROM participants WHERE event_id={} AND mobile={}".format(Event,Mobile))) > 0 :
+            return render_template('loginfail.html', errors = ["Student already Registered for the Event!"])
+
+        if runQuery("SELECT COUNT(*) FROM participants WHERE event_id={}".format(Event)) >= runQuery("SELECT participants FROM events WHERE event_id={}".format(Event)):
+            return render_template('loginfail.html', errors = ["Participants count fullfilled Already!"])
+
+        runQuery("INSERT INTO participants(event_id,fullname,email,mobile,college,branch_id) VALUES({},\"{}\",\"{}\",\"{}\",\"COEP\",\"{}\");".format(Event,Name,Email,Mobile,Branch_id))
+
+        return render_template('index.html',events = events,branchs = branch,errors=["Succesfully Registered!"])
+
+    return render_template('index.html',events = events,branchs = branch)
+    
+
+
+@app.route('/loginfail',methods=['GET'])
+def renderLoginFail():
+    return render_template('loginfail.html')
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def renderAdmin():
+    if request.method == 'POST':
+        UN = request.form['username']
+        PS = request.form['password']
+
+        cred = runQuery("SELECT * FROM admin")
+        print(cred)
+        for user in cred:
+            if UN==user[0] and PS==user[1]:
+                return redirect('/eventType')
+
+        return render_template('admin.html',errors=["Wrong Username/Password"])
+
+    return render_template('admin.html')    
+
+
+
+@app.route('/eventType',methods=['GET','POST'])
+def getEvents():
+    eventTypes = runQuery("SELECT *,(SELECT COUNT(*) FROM participants AS P WHERE T.type_id IN (SELECT type_id FROM events AS E WHERE E.event_id = P.event_id ) ) AS COUNT FROM event_type AS T;") 
+
+    events = runQuery("SELECT event_id,event_title,(SELECT COUNT(*) FROM participants AS P WHERE P.event_id = E.event_id ) AS count FROM events AS E;")
+
+    types = runQuery("SELECT * FROM event_type;")
+
+    location = runQuery("SELECT * FROM location")
+
+
+    if request.method == "POST":
+        try:
+
+            Name = request.form["newEvent"]
+            fee=request.form["Fee"]
+            participants = request.form["maxP"]
+            Type=request.form["EventType"]
+            Location = request.form["EventLocation"]
+            Date = request.form['Date']
+            runQuery("INSERT INTO events(event_title,event_price,participants,type_id,location_id,date) VALUES(\"{}\",{},{},{},{},\'{}\');".format(Name,fee,participants,Type, Location,Date))
+
+        except:
+            EventId=request.form["EventId"]
+            runQuery("DELETE FROM events WHERE event_id={}".format(EventId))
+
+    return render_template('events.html',events = events,eventTypes = eventTypes,types = types,locations = location) 
+
+
+@app.route('/eventinfo')
+def rendereventinfo():
+    events=runQuery("SELECT *,(SELECT COUNT(*) FROM participants AS P WHERE P.event_id = E.event_id ) AS count FROM events AS E LEFT JOIN event_type USING(type_id) LEFT JOIN location USING(location_id);")
+
+    return render_template('events_info.html',events = events)
+
+@app.route('/participants',methods=['GET','POST'])
+def renderParticipants():
+    
+    events = runQuery("SELECT * FROM events;")
+
+    if request.method == "POST":
+        Event = request.form['Event']
+
+        participants = runQuery("SELECT p_id,fullname,mobile,email FROM participants WHERE event_id={}".format(Event))
+        return render_template('participants.html',events = events,participants=participants)
+
+    return render_template('participants.html',events = events)
+
+def runQuery(query):
+
+    try:
+        db = mysql.connector.connect( host='localhost',database='police_station_db',user='root',password='asdafasdf')
+
+        if db.is_connected():
+            print("Connected to MySQL, running query: ", query)
+            cursor = db.cursor(buffered = True)
+            cursor.execute(query)
+            db.commit()
+            res = None
+            try:
+                res = cursor.fetchall()
+            except Exception as e:
+                print("Query returned nothing, ", e)
+                return []
+            return res
+
+    except Exception as e:
+        print(e)
+        return []
+
+    db.close()
+
+    print("Couldn't connect to MySQL")
+    return None
+
+
+if __name__ == "__main__":
+    app.run() 
---
+++ database/events.sql
@@ -0,0 +1,133 @@
+USE police_station_db;
+
+CREATE TABLE Station (
+	'station_id' VARCHAR(5),
+	'station_name' VARCHAR(15),
+	'phone_no' NUMERIC(10, 0),
+	'address' VARCHAR(30),
+	CONSTRAINT 'pk_stn' PRIMARY KEY('station_id'));
+
+CREATE TABLE 'Rank_Info' (
+	'rank' VARCHAR(20),
+	'salary' INTEGER,
+	'no_officers' INTEGER,
+	'min_exp' INTEGER,
+	'contrib_pts' INTEGER,
+	CONSTRAINT 'pk_rank' PRIMARY KEY('rank'));
+
+CREATE TABLE 'Employee' (
+	'emp_id' VARCHAR(6),
+	'name' VARCHAR(15),
+	'rank' VARCHAR(20),
+	'station_id' VARCHAR(5),
+	'date_of_join' DATE,
+	'password' VARCHAR(20),
+	CONSTRAINT 'pk_emp' PRIMARY KEY('emp_id'),
+	CONSTRAINT 'fk_emp' FOREIGN KEY('station_id') REFERENCES Station('station_id'),
+	CONSTRAINT 'fk_emprank' FOREIGN KEY('rank') REFERENCES Rank_Info('rank'));
+
+CREATE TABLE 'Detained' (
+	'd_id' VARCHAR(5),
+	'emp_id' VARCHAR(6),
+	'name' VARCHAR(20),
+	'station_id' VARCHAR(5),
+	'descpt' VARCHAR(50),
+	'date_in' DATE,
+	CONSTRAINT 'pk_det' PRIMARY KEY('d_id'),
+	CONSTRAINT 'fk_det' FOREIGN KEY('station_id') REFERENCES Station('station_id')
+	CONSTRAINT 'fk_detemp' FOREIGN KEY('emp_id') REFERENCES Employee('emp_id')
+);
+
+CREATE TABLE 'Visitor' (
+	'vis_id' VARCHAR(5),
+	'name' VARCHAR(20),
+	'station_id' VARCHAR(5),
+	'purpose' VARCHAR(40),
+	'entry_date' DATE,
+	'time_in' TIME,
+	'time_out' TIME,
+	CONSTRAINT 'pk_vis' PRIMARY KEY('vis_id'),
+	CONSTRAINT 'fk_vis' FOREIGN KEY('station_id') REFERENCES Station('station_id'));
+
+CREATE TABLE 'Complaint' (
+	'comp_id' VARCHAR(5),
+	'station_id' VARCHAR(5),
+	'date_filed' DATE,
+	'filed_by' VARCHAR(20),
+	'phone_no' NUMERIC(10,0),
+	'descpt' VARCHAR(50),
+	'status' VARCHAR(9),
+	CONSTRAINT 'pk_comp' PRIMARY KEY('comp_id'),
+	CONSTRAINT 'fk_comp' FOREIGN KEY('station_id') REFERENCES Station('station_id'));
+
+CREATE TABLE 'Fine_Info' (
+	'matter' VARCHAR(20),
+	'payment' INTEGER,
+	CONSTRAINT 'fk_inf' PRIMARY KEY('matter'));
+
+CREATE TABLE 'Fine' (
+	'fine_id' VARCHAR(5),
+	'police_id' VARCHAR(6),
+	'aadhar_id' NUMERIC(12, 0),
+	'matter' VARCHAR(20),
+	'fined_date' DATE,
+	CONSTRAINT 'pk_fine' PRIMARY KEY('fine_id'),
+	CONSTRAINT 'fk_fine' FOREIGN KEY('police_id') REFERENCES Employee('emp_id'),
+	CONSTRAINT 'fk_pay' FOREIGN KEY('matter') REFERENCES Fine_Info('matter'));
+
+INSERT INTO Station VALUES('A1415', 'Vijayanagar', 9428302313, 'Vijayanager, Bangalore');
+INSERT INTO Station VALUES('A1416', 'Kengeri', 9428302314, 'Kengeri, Bangalore');
+INSERT INTO Station VALUES('A1417', 'Rajajinagar', 9428302315, 'Rajajinagar, Bangalore');
+
+INSERT INTO Rank_Info VALUES('Circle Inspector', 50000, 5, 10, 10);
+INSERT INTO Rank_Info VALUES('Inspector', 40000, 10, 8, 8);
+INSERT INTO Rank_Info VALUES('Sub-Inspector', 30000, 15, 6, 6);
+INSERT INTO Rank_Info VALUES('Constable', 20000, 20, 4, 4);
+INSERT INTO Rank_Info VALUES('Head Constable', 25000, 15, 5, 5);
+
+
+INSERT INTO Employee VALUES('U143', 'Ajit', 'Circle Inspector', 'A1415', '20-Feb-2010', 'arkh123*');
+INSERT INTO Employee VALUES('U144', 'John', 'Inspector', 'A1416', '15-Mar-2012', 'john123*');
+INSERT INTO Employee VALUES('U145', 'Lisa', 'Sub-Inspector', 'A1415', '10-Jul-2015', 'lisa456*');
+INSERT INTO Employee VALUES('U146', 'David', 'Constable', 'A1417', '05-Nov-2018', 'david789*'); 
+INSERT INTO Employee VALUES('U147', 'Emma', 'Constable', 'A1416', '12-Jan-2020', 'emma123*'); 
+INSERT INTO Employee VALUES('U148', 'Michael', 'Head Constable', 'A1415', '25-Sep-2016', 'michael456*'); 
+INSERT INTO Employee VALUES('U149', 'Sophia', 'Sub-Inspector', 'A1415', '18-Apr-2014', 'sophia789*');
+
+INSERT INTO Detained VALUES('D001', 'U143', 'Kamal', 'A1415', 'Theft', '20-Feb-2010');
+INSERT INTO Detained VALUES('D002', 'U144', 'Rahul', 'A1416', 'Murder', '13-Jan 2013');
+INSERT INTO Detained VALUES('D003', 'U145', 'Ravi', 'A1415', 'Robbery', '05-Jul-2019'); 
+INSERT INTO Detained VALUES('D004', 'U146', 'Sara', 'A1417', 'Assault', '10-Oct-2020'); 
+INSERT INTO Detained VALUES('D005', 'U147', 'Alex', 'A1416', 'Drug Possession', '15-Feb-2021'); 
+INSERT INTO Detained VALUES('D006', 'U148', 'Olivia', 'A1415', 'Fraud', '20-May-2017'); 
+INSERT INTO Detained VALUES('D007', 'U149', 'Daniel', 'A1415', 'Burglary', '25-Sep-2018');
+
+INSERT INTO Visitor VALUES('V001', 'John Doe', 'A1415', 'Meeting', '2022-01-01', '09:00:00', '10:00:00'); 
+INSERT INTO Visitor VALUES('V002', 'Jane Smith', 'A1416', 'Delivery', '2022-01-02', '14:30:00', '15:30:00'); 
+INSERT INTO Visitor VALUES('V003', 'Mike Johnson', 'A1417', 'Inquiry', '2022-01-03', '11:00:00', '12:00:00'); 
+INSERT INTO Visitor VALUES('V004', 'Emily Brown', 'A1415', 'Appointment', '2022-01-04', '16:00:00', '17:00:00'); 
+INSERT INTO Visitor VALUES('V005', 'Alex Wilson', 'A1416', 'Complaint', '2022-01-05', '10:30:00', '11:30:00');
+
+INSERT INTO Complaint VALUES('C001', 'A1415', '2022-01-01', 'John Doe', 9428302313, 'Robbery', 'Pending'); 
+INSERT INTO Complaint VALUES('C002', 'A1416', '2022-01-02', 'Jane Smith', 9428302314, 'Fraud', 'Pending');
+INSERT INTO Complaint VALUES('C003', 'A1417', '2022-01-03', 'Mike Johnson', 9428302315, 'Assault', 'Pending');
+INSERT INTO Complaint VALUES('C004', 'A1415', '2022-01-04', 'Emily Brown', 9428302313, 'Theft', 'Pending');
+
+-- --------------------------------------------------------------------------------------------------------
+
+
+-- -----------------------------------------------------------------------------
+
+
+
+
+CREATE TABLE `admin`(
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+);
+
+INSERT INTO `admin` VALUES
+('Admin1','password1'),
+('Admin2','password2');
+
+----------------------------------------------------------------------------------
